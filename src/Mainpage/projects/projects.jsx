e71bc55fed01b4c6fd588f869d23208180f87299
Abhinav <ayushabhinav19@gmail.com>
2025-04-09 23:10:48 +0530

    update

--- src/Mainpage/projects/projects.jsx
+++ src/Mainpage/projects/projects.jsx
@@ -4,9 +4,9 @@ import "./projects.css"
 export let Projects=({color,textcolor})=>{
 
     const projectimg=["Images/netflix.png","https://prod.cloud.rockstargames.com/ugc/gta5mission/gAIT3vi7b0mn4tZUaTVw6g/2_0.jpg","Images/Pokedexphoto1.png"]
-    const projectname=["Netflix Clone","To-Do List","Pokedex"]
-    const projectbio=["A UI Clone of Netflix with React","Simple and efficient task manager","A React webpage displaying Pokemon data"]
-    const githublink=["https://github.com/ayushabhinav19/Netflix-login-clone","https://github.com/ayushabhinav19/Pokedex"]
+    const projectname=["Netflix Clone","SpaceX","Pokedex"]
+    const projectbio=["A UI Clone of Netflix with React","An Api integrated spacex website","A React webpage displaying Pokemon data"]
+    const githublink=["https://github.com/ayushabhinav19/Netflix-login-clone","https://github.com/ayushabhinav19/Spacex","https://github.com/ayushabhinav19/Pokedex"]
 
     return(
         <section  className="projectsection" id="projects" style={{backgroundColor:color}}>
@@ -19,10 +19,9 @@ export let Projects=({color,textcolor})=>{
             <div className="projectgrid">
                 <Card image={projectimg[0]} title={projectname[0]} projectabout={projectbio[0]} skill1={"React"} skill2={"Css"} gitlink={githublink[0]} ></Card>
                 
-                <Card image={projectimg[1]} title={projectname[1]} projectabout={projectbio[1]} skill1={"React"} skill2={"LocalStorage"}></Card> 
+                <Card image={projectimg[1]} title={projectname[1]} projectabout={projectbio[1]} skill1={"React"} skill2={"LocalStorage"} wwebsite={"https://spacex.fourrnexus.com/"}></Card> 
                 
-                <Card image={projectimg[2]} title={projectname[2]} projectabout={projectbio[2]} skill1={"JS"} skill2={"Css"}
-                gitlink={githublink[1]}></Card>
+                <Card image={projectimg[2]} title={projectname[2]} projectabout={projectbio[2]} skill1={"JS"} skill2={"Css"} gitlink={githublink[1]} ></Card>
             </div>
 
         </section>
